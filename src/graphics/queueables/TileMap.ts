0d7627b4a248d149d6891348308d024b422fa188
Matt Klein <mklein246@gmail.com>
2020-08-29 00:05:20 -0700

    some notes added

--- src/graphics/queueables/TileMap.ts
+++ src/graphics/queueables/TileMap.ts
@@ -4,28 +4,6 @@ import { Queueable } from "./Queueable";
 import { Vector } from "../../util/Vector";
 import { getRange } from "../../util/helpers";
 
-/* Notes / further musings.
-  - Need a plan for this section, and how it relates to RenderQueue
-  - Do I combine the two of them? Wouldn't be too hard to just stick a set for each layer onto the TileMap
-  - Or I could put a TileMap into the RenderQueue
-  - Either way, I'm feeling fairly certain that one should control the other. I shouldn't have to make a call to both of them for every action.
-  - TileMap's strengths are mostly going to be for manipulation stuff. Provides easier / faster ways to look up what is where.
-  - So. Structures we have going:
-    - Media[] is where ImageSources are stored.
-    - Sprites[] is where created Sprites are stored (even ones that are not currently used).
-    - RenderQueue is CURRENTLY where everything that is currently on the map is stored.
-    - TileMap is a place to more easily organize what is on the map. Provides for 0(1) lookup for occupants of a specific tile.
-    - Will there be a need to grab sprites by their id? I'm actually not sure there will be. So TileMap is def the way to go.
-    - Get some decent manipulation methods on there. Maybe some way to manipulate a group based on a selection.
-    - I think in the interest of single state and such, that TileMap should be the source of truth. RenderQueue (or 
-      just the Renderer if the queue is no longer needed) can get it's info from the sets I'll weave into TileMap.
-    - This... sounds like a good idea. Organizationally, it will provide:
-      - Easy way to look at what Sprites are at specific locations on the map.
-      - Efficient lists (sets) of what Sprites are on the map. (don't have to iterate through all the nulls = faster.)
-    - What else will be needed? I'm... not sure. Look into how pixels on the canvas are used in the first place. Might be something missed.
-    - Hey wait is this the damn TileMap? A collection of them I guess. Oy. I suppose a map would be getting it into a json.
-*/
-
 export class TileMap extends Queueable {
   private graph: (Sprite | null)[];
   private manifest: SpriteManifest;
--- src/input/InputHandler.ts
+++ src/input/InputHandler.ts
@@ -22,6 +22,24 @@ export class InputHandler {
   }
 
   mouseDown(e: MouseEvent) {
+    /*
+      These functions right here are where this should start next. We have a place set up to catch input.
+      What we need is a decent interface to feed that input into for the actions.
+      We already know what to do for the most part. The basic loop won't be different from SnapShot.
+      - ondown, create action and set local state for it. set it to currentAction
+      - onmove, if currentAction exists, call an update on it.
+      - onup, resolve the action and delete it.
+
+      - The "tools" as we know them will actually be tool ACTIONS. We'll need a factory to create them.
+      - We'll need a good way to connect the tools to the project. If we're assuming there will be just the one TileMap,
+        a reference to it should do the trick. We'll need some better tools to manipulate that data on that also.
+      - Again, basically the implementations being used in SnapShot should work well here. There are differences, but 
+        there is more than enough there to get I think just about all the way through the complicated stuff here.
+      - One possible hiccup: selection. From a data standpoint, should be a cinch: just a set of numbers. Or even an boolean array acting as a mask.
+      - Buuuuut the visuals might be a bit annoying. Gonna have to figure out how to draw along the borders of selected blocks efficiently.
+      - Might be possible with just the standard marching squares, filling in the blocks to create a mask. I feel like there should be a
+        much, much faster way though.
+    */
     const input: MouseInput = parseMouseInput(e, this.state.getTranslateData());
     if (e.button === 1) {
       this.state.middleClickTool.start(input);
